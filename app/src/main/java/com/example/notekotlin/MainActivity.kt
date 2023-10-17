ef0b2b3b8cad79471e04abb96795780dceadba6b
Monica <monica.walentek@gmail.com>
2023-10-17 16:48:11 +0200

    ✨[FEAT]: permit to create new note and see them

--- app/src/main/java/com/example/notekotlin/MainActivity.kt
+++ app/src/main/java/com/example/notekotlin/MainActivity.kt
@@ -1,43 +1,60 @@
 package com.example.notekotlin
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.notekotlin.ui.theme.NoteKotlinTheme
+import androidx.compose.runtime.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            NoteKotlinTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+        setContentView(R.layout.activity_main)
+        val btnEditNote = findViewById<ImageButton>(R.id.imageButton)
+        btnEditNote.setOnClickListener {
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            alertDialogBuilder.setTitle("Ajouter une note")
+
+            // Créer un champ de saisie (EditText) dans la boîte de dialogue
+            val inputEditText = EditText(this)
+            inputEditText.hint = "Saisissez votre note"
+
+            alertDialogBuilder.setView(inputEditText)
+
+            alertDialogBuilder.setPositiveButton("Enregistrer") { _, _ ->
+                val enteredText = inputEditText.text.toString()
+                if (enteredText.isNotEmpty()) {
+                    // Créer un TextView pour afficher la note
+                    val noteTextView = TextView(this)
+                    noteTextView.text = enteredText
+
+                    // Obtenir la zone des notes LinearLayout
+                    val zoneDesNotes = findViewById<LinearLayout>(R.id.zone_des_notes)
+
+                    // Ajouter le TextView à la zone des notes
+                    zoneDesNotes.addView(noteTextView)
                 }
             }
+
+            alertDialogBuilder.setNegativeButton("Annuler") { _, _ ->
+                // Rien ?
+            }
+
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteKotlinTheme {
-        Greeting("Android")
-    }
 }
