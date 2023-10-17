1a09ec360d7624b525a68a388ab6c9652fad806e
Monica <monica.walentek@gmail.com>
2023-10-17 17:02:33 +0200

    ✨[FEAT]: permit to edit note

--- app/src/main/java/com/example/notekotlin/MainActivity.kt
+++ app/src/main/java/com/example/notekotlin/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.notekotlin
 import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
@@ -22,6 +24,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnEditNote = findViewById<ImageButton>(R.id.imageButton)
+        var textViewIdCounter = 0
+
         btnEditNote.setOnClickListener {
             val alertDialogBuilder = AlertDialog.Builder(this)
             alertDialogBuilder.setTitle("Ajouter une note")
@@ -38,6 +42,17 @@ class MainActivity : ComponentActivity() {
                     // Créer un TextView pour afficher la note
                     val noteTextView = TextView(this)
                     noteTextView.text = enteredText
+                    noteTextView.gravity = Gravity.CENTER
+                    noteTextView.textSize = 24f // Taille du texte en sp (vous pouvez ajuster la taille selon vos préférences)
+
+                    // Générer un ID unique pour le TextView
+                    val textViewId = View.generateViewId()
+                    noteTextView.id = textViewId
+
+                    // Définir un OnClickListener pour le TextView
+                    noteTextView.setOnClickListener {
+                        showEditDialog(noteTextView)
+                    }
 
                     // Obtenir la zone des notes LinearLayout
                     val zoneDesNotes = findViewById<LinearLayout>(R.id.zone_des_notes)
@@ -56,5 +71,30 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    fun showEditDialog(textView: TextView) {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("Éditer la note")
 
+        // Créer un champ de saisie (EditText) dans la boîte de dialogue
+        val inputEditText = EditText(this)
+        inputEditText.hint = "Modifiez votre note"
+        inputEditText.setText(textView.text)
+
+        alertDialogBuilder.setView(inputEditText)
+
+        alertDialogBuilder.setPositiveButton("Enregistrer") { _, _ ->
+            val editedText = inputEditText.text.toString()
+            if (editedText.isNotEmpty()) {
+                // Mettre à jour le texte du TextView avec le texte édité
+                textView.text = editedText
+            }
+        }
+
+        alertDialogBuilder.setNegativeButton("Annuler") { _, _ ->
+            // Rien.
+        }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
 }
